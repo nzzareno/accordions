c976c9dbc1153b689499de2941e31a7b0789f9ec
GarmaNazareno <garmatorresnazareno@gmail.com>
2022-03-25 17:38:09 -0300

    Updates

---
+++ precache-manifest.c367c88fb2340e349dfa235b60e7794c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e691b7fac60c82026ebda7a8085d57d2",
+    "url": "/accordions/index.html"
+  },
+  {
+    "revision": "bfbb4af15eea2ae53262",
+    "url": "/accordions/static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordions/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordions/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbb4af15eea2ae53262",
+    "url": "/accordions/static/js/main.3cfb76db.chunk.js"
+  },
+  {
+    "revision": "08d1fa688f889ade02d0",
+    "url": "/accordions/static/js/runtime-main.72f8bc7f.js"
+  }
+]);
